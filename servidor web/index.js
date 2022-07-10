7ffc4c01feaac987e949e2e13d29eff449cc8723
mauronvrr19 <87575276+mauronvrr19@users.noreply.github.com>
2022-07-10 15:56:35 -0300

    Add files via upload

---
+++ servidor web/index.js
@@ -0,0 +1,45 @@
+// servidor
+
+const { response } = require('express');
+const express = require('express');
+const path = require('path')
+const Contener = require("./contenedor")
+
+const nombreArchivo =  new Contener("./productos.txt")
+
+const app = express();
+
+const puerto = 8080;
+const server = app.listen(puerto, () =>
+  console.log('Server Up en puerto', puerto)
+);
+
+server.on('error', (err) => {
+  console.log('ERROR =>', err);
+});
+
+// rutas
+
+app.get ("/productos", async (req, res)=>{
+  let prod
+    try {
+      prod = await nombreArchivo.getData()
+        } 
+            catch (error) {
+console.log(error)
+    }
+res.send(prod)
+  })
+
+
+  app.get ("/productosRandom", async(req, res)=>{
+    let producto_azar
+     try {
+         const azar = Math.floor(Math.random()*3);
+      producto_azar = await nombreArchivo.getById(azar)
+     } catch (error) {
+       console.log(error)
+     }
+   res.send(producto_azar)
+   })
+  
